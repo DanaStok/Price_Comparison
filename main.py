5d60432a31d927510fc337e803dd9ebfd090c77f
dana.stok <dana.stok@post.runi.ac.il>
2024-04-26 13:21:36 +0300

    Mostly working

--- main.py
+++ main.py
@@ -5,12 +5,23 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from fastapi.middleware.cors import CORSMiddleware
+import re
 
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Allows all origins for the Next.js server
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
 def setup_driver():
-    # Path to the ChromeDriver
+    # Path to the ChromeDriverpip install fastapi uvicorn
+
     PATH = "./chromedriver"  # Update this path
     service = Service(executable_path=PATH)
     options = Options()
@@ -31,9 +42,9 @@ async def search_all_sites(product_name: str):
         driver.quit()
     
     return {
-        "BestBuy": {"Item": results[0][1], "Price": results[0][2]},
-        "Walmart": {"Item": results[1][1], "Price": results[1][2]},
-        "Newegg": {"Item": results[2][1], "Price": results[2][2]}
+        "BestBuy": {"Item": results[0][1], "Price": results[0][2], "URL": results[0][3]},
+        "Walmart": {"Item": results[1][1], "Price": results[1][2], "URL": results[1][3]},
+        "Newegg": {"Item": results[2][1], "Price": results[2][2], "URL": results[2][3]}
     }
     
 def click_us_link(driver, url, website):
@@ -52,36 +63,48 @@ def click_us_link(driver, url, website):
     except Exception as e:
         print(f"The United States link was not clickable or not found within the timeout period: {str(e)}")
         
-        
+
+def clean_price(price_str):
+    # This regular expression removes any characters that are not digits, period, or comma
+    clean_price = re.sub(r'[^\d.,]', '', price_str)
+    return clean_price
+    
 def search_bestbuy(driver, product_name):
     url = f'https://www.bestbuy.com/site/searchpage.jsp?st={product_name}'
     driver.get(url)
     click_us_link(driver,url,"BestBuy")
     
     try:
-        product = WebDriverWait(driver, 10).until(
+        product_element = WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located((By.CSS_SELECTOR, '.sku-title a'))
-        ).text
+        )
+        product = product_element.text
+        product_url = product_element.get_attribute('href')
+        print(f"BestBuy URL: {product_url}")
         price = WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located((By.CSS_SELECTOR, '.pricing-price  div[data-testid="large-price"] .priceView-customer-price > span:nth-child(1)'))
         ).text
-        return ('Bestbuy', product, price)
+        return ('Bestbuy', product, clean_price(price), product_url)
     except:
-        return ('Bestbuy', None, None)
+        return ('Bestbuy', None, None, None)
 
 def search_walmart(driver, product_name):
     url = f'https://www.walmart.com/search/?query={product_name}'
     driver.get(url)
     try:
-        product = WebDriverWait(driver, 10).until(
+        product= WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located((By.CSS_SELECTOR, 'span[data-automation-id="product-title"]'))
         ).text
+        product_url = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'a.absolute.w-100.h-100.z-1.hide-sibling-opacity'))
+        ).get_attribute('href')
+        print(f"Walmart URL: {product_url}")
         price = WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located((By.CSS_SELECTOR, 'div[data-automation-id="product-price"] div[aria-hidden="true"]'))
         ).text
-        return ('Walmart', product, price)
+        return ('Walmart', product, clean_price(price), product_url)
     except:
-        return ('Walmart', None, None)
+        return ('Walmart', None, None, None)
 
 def search_newegg(driver, product_name):
     url = f'https://www.newegg.com/p/pl?d={product_name}'
@@ -89,15 +112,18 @@ def search_newegg(driver, product_name):
     click_us_link(driver,url,"Newegg")
     
     try:
-        product = WebDriverWait(driver, 10).until(
+        product_element = WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located((By.CSS_SELECTOR, '.item-title'))
-        ).text
+        )
+        product = product_element.text
+        product_url = product_element.get_attribute('href')
+        print(f"Newegg URL: {product_url}")
         price = WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located((By.CSS_SELECTOR, '.price-current'))
         ).text
-        return ('Newegg', product, price)
+        return ('Newegg', product, clean_price(price), product_url)
     except:
-        return ('Newegg', None, None)
+        return ('Newegg', None, None, None)
 
 if __name__ == "__main__":
     import uvicorn
--- price-comparison/app/page.tsx
+++ price-comparison/app/page.tsx
@@ -1,8 +1,68 @@
-import Image from 'next/image'
+'use client';
+import React, { useState } from 'react';
 
-export default function Home() {
+function Search() {
+  const [productName, setProductName] = useState('');
+  const [searchResults, setSearchResults] = useState([]); // State to store search results
+
+  const handleSearch = async (e) => {
+    e.preventDefault(); // Prevent the default form submit action
+    try {
+      const response = await fetch('http://127.0.0.1:8000/search/' + productName, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      const data = await response.json();
+      console.log('Response data from the backend:', data);
+
+      // Prepare an array of results for the table including URLs
+      const results = [
+        { company: 'BestBuy', productName: data.BestBuy.Item, price: data.BestBuy.Price, url: data.BestBuy.URL},
+        { company: 'Walmart', productName: data.Walmart.Item, price: data.Walmart.Price, url: data.Walmart.URL},
+        { company: 'Newegg', productName: data.Newegg.Item, price: data.Newegg.Price,  url: data.Newegg.URL}
+      ];
+      setSearchResults(results); // Update the state with the new results
+
+    } catch (error) {
+      console.error('There was an error!', error);
+    }
+  };
+
   return (
-    <main><h1>Hello World!</h1></main>
-  )
+    <div>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={productName}
+          onChange={(e) => setProductName(e.target.value)}
+          placeholder="Enter a product name"
+        />
+        <button type="submit">Search</button>
+      </form>
+      {searchResults.length > 0 && (
+        <table className='table table-bordered'>
+          <thead>
+            <tr>
+              <th>Company</th>
+              <th>Product Name</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {searchResults.map((result, index) => (
+              <tr key={index}>
+                <td>{result.company}</td>
+                <td><a href={result.url} target="_blank" rel="noopener noreferrer">{result.productName}</a></td>
+                <td>${result.price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 }
-    
+
+export default Search;
